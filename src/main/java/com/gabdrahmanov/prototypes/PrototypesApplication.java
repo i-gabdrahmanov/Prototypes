b0ba506b5738bdcaeb2cc3d376bcf26b0ffc96ca
i-gabdrahmanov <alex.geo.kpp@icloud.com>
2024-07-19 09:54:42 +0300

    init

---
+++ src/main/java/com/gabdrahmanov/prototypes/PrototypesApplication.java
@@ -0,0 +1,13 @@
+package com.gabdrahmanov.prototypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypesApplication.class, args);
+    }
+
+}
