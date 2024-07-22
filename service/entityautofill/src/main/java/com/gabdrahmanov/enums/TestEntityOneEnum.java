36ca3a812d4dff7b634f79e34395d3cbb2395675
i-gabdrahmanov <alex.geo.kpp@icloud.com>
2024-07-22 17:05:40 +0300

    add test 1 enum

---
+++ service/entityautofill/src/main/java/com/gabdrahmanov/enums/TestEntityOneEnum.java
@@ -0,0 +1,25 @@
+package com.gabdrahmanov.enums;
+
+import com.gabdrahmanov.entity.DictionaryEnum;
+import com.gabdrahmanov.entity.RegistryEntity;
+import com.gabdrahmanov.entity.TestEntityOne;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TestEntityOneEnum implements DictionaryEnum<TestEntityOne> {
+    ONE(1L, "First", 1000),
+    TWO(2L, "Second", 2000),
+    THREE(3L, "Third", 3000);
+
+    private final Long code;
+    private final String name;
+    private final int cost;
+
+
+    @Override
+    public Class<? extends RegistryEntity> getEntityType() {
+        return TestEntityOne.class;
+    }
+}
